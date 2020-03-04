c5d75ffdc9bb3ab641adea9383b2d9ff3d188202
rosi <dejanira.araiza.i@gmail.com>
2020-03-04 11:23:02 +0800

    Reading travis again

--- ci_travis.sh
+++ ci_travis.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir -p /home/packml_ws/src
-cp -r /home/packml_ros2 /home/packml_ws/src
-cd /home/packml_ws/
-apt update && apt install curl gnupg2 lsb-release
+sudo mkdir -p /home/packml_ws/src
+sudo cp -r $TRAVIS_BUILD_DIR/packml_ros2 /home/packml_ws/src
+sudo cd /home/packml_ws/
+sudo apt update && sudo apt install curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
-sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-apt update
-apt install ros-$ROS_DISTRO-desktop
+sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+sudo apt update
+sudo apt install ros-$ROS_DISTRO-desktop
 source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build
