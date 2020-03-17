f115fcf1cba6af88bfdbd28c6a5586aa3179e59f
rosi <dejanira.araiza.i@gmail.com>
2020-03-17 13:35:15 +0800

    Separated personalized test script from main travis bash script

--- run_tests.sh
+++ run_tests.sh
@@ -2,12 +2,13 @@
 # -*- indent-tabs-mode: nil -*-
 
 export ROS_CI_DIR=$(dirname ${BASH_SOURCE:-$0}) #path to directory running the current script
+export ROS_WS=${ROS_WS:-/root/ros_ws} #location of workspace
 
 #Loading helper functions
 source ${ROS_CI_DIR}/util.sh
 
 #Run tests and code coverage
-travis_run_wait ros2 run packml_sm packml_sm_utest #./run_tests.sh
+travis_run_wait ros2 run packml_sm packml_sm_utest
 travis_run_wait ros2 run packml_ros packml_ros_utest
 travis_run_wait python3 -m coverage run $ROS_WS/src/packml_ros2/packml_plc/packml_plc/test_packml_plc_listener.py
 travis_run_wait python3 -m coverage run $ROS_WS/src/packml_ros2/packml_plc/packml_plc/test_packml_plc_sender.py
