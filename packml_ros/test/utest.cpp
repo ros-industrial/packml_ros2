c8240430af00e73f68229d85fa689d265360b9d1
Chen Bainian <brian97cbn@gmail.com>
2020-05-14 17:25:19 +0800

    Fix too much stdout during testing causing CI failing
    Signed-off-by: Chen Bainian <brian97cbn@gmail.com>

--- packml_ros/test/utest.cpp
+++ packml_ros/test/utest.cpp
@@ -579,6 +579,7 @@ int main(int argc, char ** argv)
   // qtWorker(argc, argv);
   while (NULL == QCoreApplication::instance()) {
     printf("Waiting for QCore application to start\n");
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
   thr.detach();
   testing::InitGoogleTest(&argc, argv);
